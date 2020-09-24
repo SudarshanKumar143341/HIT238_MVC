c4efb91af7dc1ea1d8803792960937456316cdc2
DESKTOP-8CK243U\sudar <sudarshan.bhattarai@students.cdu.edu.au>
2020-09-24 18:14:12 +0930

    updated function to increase score when correct anser is selected in quiz2

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,7 +1,8 @@
 var score=document.querySelector(".score_screen").value;
 document.querySelector(".score_screen").value=score;
-document.querySelector(".cat").addEventListener("click", function(){
+document.querySelector(".cat1, .dog1").addEventListener("click", function(){
   score++;
+//  sessionStorage.setItem("score", score;
   document.querySelector(".score_screen").value=score;
   document.body.style.background="green";
-})
+});
